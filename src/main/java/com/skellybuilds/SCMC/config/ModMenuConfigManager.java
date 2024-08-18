355944e716a8173350b9ea4a2782bf2ee6086d22
SkellyBuilds <munashegaming@gmail.com>
2024-08-18 23:25:58 +0200

    DO NOT BUILD - changing computers

--- src/main/java/com/skellybuilds/SCMC/config/ModMenuConfigManager.java
+++ src/main/java/com/skellybuilds/SCMC/config/ModMenuConfigManager.java
@@ -11,17 +11,65 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
 public class ModMenuConfigManager {
 	private static File file;
+	//private static  engine;
+
+	private static JsonObject jsObjtoJSON(Value value, Gson gson) {
+		JsonObject jsonObject = new JsonObject();
+
+		for (String key : value.getMemberKeys()) {
+			Value memberValue = value.getMember(key);
+			JsonElement jsonElement = gson.toJsonTree(memberValue.as(Object.class));
+			jsonObject.add(key, jsonElement);
+		}
+
+		return jsonObject;
+	}
+
 
 	private static void prepareConfigFile() {
 		if (file != null) {
 			return;
 		}
-		file = new File(FabricLoader.getInstance().getConfigDir().toFile(), SCMC.MOD_ID + ".json");
+
+		file = new File(FabricLoader.getInstance().getConfigDir().toFile(), SCMC.MOD_ID + ".js");
+	}
+
+	public static void convertToJS(){
+		Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
+		File fileS;
+
+        try {
+			fileS = new File(FabricLoader.getInstance().getConfigDir().toFile(), SCMC.MOD_ID + ".json");
+            BufferedReader br = new BufferedReader(new FileReader(fileS));
+			JsonObject json = new JsonParser().parse(br).getAsJsonObject();
+			String jsConfig = "module.exports = " + gson.toJson(json);
+
+			try (FileWriter writer = new FileWriter(file)) {
+				writer.write(jsConfig);
+			}
+
+			boolean deleted = fileS.delete();
+			if(!deleted) {
+				SCMC.LOGGER.error("Unable to delete the previous config file");
+			}
+
+			load();
+
+		} catch (IOException e) {
+            SCMC.LOGGER.error("Unable to read previous json");
+        }
+
 	}
 
 	public static void initializeConfig() {
@@ -34,11 +82,24 @@ public class ModMenuConfigManager {
 
 		try {
 			if (!file.exists()) {
-				save();
+				if(new File(FabricLoader.getInstance().getConfigDir().toFile(), SCMC.MOD_ID + ".json").exists()){
+					convertToJS();
+					return;
+				} else {
+					save();
+				}
 			}
 			if (file.exists()) {
-				BufferedReader br = new BufferedReader(new FileReader(file));
-				JsonObject json = new JsonParser().parse(br).getAsJsonObject();
+				//BufferedReader br = new BufferedReader(new FileReader(file));
+				Gson gson = new Gson();
+				JsonObject json;
+				Path filePath = Paths.get(file.getAbsolutePath());
+				String scriptContent = Files.readString(filePath);
+
+				try (Context context = Context.create("js")) {
+					context.eval("js",scriptContent);
+					json = jsObjtoJSON(context.getBindings("js").getMember("module").getMember("exports"), gson);
+				}
 
 				for (Field field : ModMenuConfig.class.getDeclaredFields()) {
 					if (Modifier.isStatic(field.getModifiers()) && Modifier.isFinal(field.getModifiers())) {
@@ -58,7 +119,7 @@ public class ModMenuConfigManager {
 					}
 				}
 			}
-		} catch (FileNotFoundException | IllegalAccessException e) {
+		} catch (IllegalAccessException | IOException e) {
 			System.err.println("Couldn't load Mod Menu configuration file; reverting to defaults");
 			e.printStackTrace();
 		}
@@ -66,6 +127,41 @@ public class ModMenuConfigManager {
 
 	@SuppressWarnings("unchecked")
 	public static void save() {
+		prepareConfigFile();
+
+		JsonObject config = new JsonObject();
+
+		try {
+			for (Field field : ModMenuConfig.class.getDeclaredFields()) {
+				if (Modifier.isStatic(field.getModifiers()) && Modifier.isFinal(field.getModifiers())) {
+					if (BooleanConfigOption.class.isAssignableFrom(field.getType())) {
+						BooleanConfigOption option = (BooleanConfigOption) field.get(null);
+						config.addProperty(field.getName().toLowerCase(Locale.ROOT), ConfigOptionStorage.getBoolean(option.getKey()));
+					} else if (StringSetConfigOption.class.isAssignableFrom(field.getType())) {
+						StringSetConfigOption option = (StringSetConfigOption) field.get(null);
+						JsonArray array = new JsonArray();
+						ConfigOptionStorage.getStringSet(option.getKey()).forEach(array::add);
+						config.add(field.getName().toLowerCase(Locale.ROOT), array);
+					}
+				}
+			}
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
+		String jsonString = "module.exports = " + GSON.toJson(config);
+
+
+
+		try (FileWriter fileWriter = new FileWriter(file)) {
+			fileWriter.write(jsonString);
+		} catch (IOException e) {
+			System.err.println("Couldn't save Mod Menu configuration file");
+			e.printStackTrace();
+		}
+	}
+	public static void saveAsJSON() {
 		prepareConfigFile();
 
 		JsonObject config = new JsonObject();
